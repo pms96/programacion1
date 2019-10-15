e081f45e503ea32606897ff73160c4920a57a4c1
diurno <diurno@pc-aula>
2019-10-15 13:30:04 +0200

    bloque1

---
+++ src/capitulo5/Ejercicio4Bloque3Capitulo5.java
@@ -0,0 +1,34 @@
+package capitulo5;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio4Bloque3Capitulo5 {
+
+	public static void main(String[] args) {
+		int numeros[] = new int[150];
+		int i = 0, sumaPares = 0, sumaImpares = 0; 
+
+		// Inicializaci�n de los valores del array
+		for ( i = 0; i < numeros.length; i++) {
+			numeros[i] = i + 1;
+			numeros [i] = (int) Math.round(Math.random() * 100);
+			System.out.println(i + ": " + numeros[i]);
+			if ( numeros[i] % 2 == 0) {
+				sumaPares += numeros[i] ;
+			} 
+			else {
+				sumaImpares += numeros[i];
+			}
+		}
+		
+		//suma de positivos y negativos
+		//for ( i = numeros.length - 1 ; i > -1; i--) {			
+			
+			
+		//}
+
+		System.out.println("La suma de los numeros positivos es: " + sumaPares);
+		System.out.println("La suma de los numeros negativos es: " + sumaImpares);
+	}
+
+}
---
+++ src/capitulo5/Ejercicio5Bloque1Capitulo5.java
@@ -0,0 +1,9 @@
+package capitulo5;
+
+public class Ejercicio5Bloque1Capitulo5 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
